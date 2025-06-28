f59fe728b191f3b91b3cb559412b4f29930b775a
tbanjelas <tbmanjelas@gmail.com>
2025-06-28 17:52:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Contoh script untuk interaksi atau animasi sederhana
+document.addEventListener("DOMContentLoaded", () => {
+  const hero = document.getElementById("hero");
+  hero.addEventListener("click", () => {
+    alert("Selamat datang di Taman Baca Anjelas!");
+  });
+});
