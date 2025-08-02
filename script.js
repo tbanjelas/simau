72f481b290153ef091155069c51ec64400057287
tbanjelas <tbmanjelas@gmail.com>
2025-08-02 16:28:54 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleDark = document.querySelector('#dark-toggle');
-  toggleDark.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-  });
-});
